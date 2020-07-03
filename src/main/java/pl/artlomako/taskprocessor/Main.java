47e9c60fe473e8f32e0dcfbda7c3c37e0842f1a9
Artur Lomako <lomako.artur@gmail.com>
2020-07-03 16:02:22 +0200

    Implement Min.main() to run all together

--- src/main/java/pl/artlomako/taskprocessor/Main.java
+++ src/main/java/pl/artlomako/taskprocessor/Main.java
@@ -1,13 +1,29 @@
 package pl.artlomako.taskprocessor;
 
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import pl.artlomako.taskprocessor.task.TaskConsumer;
+import pl.artlomako.taskprocessor.task.TaskFactory;
+import pl.artlomako.taskprocessor.task.TaskProducer;
+import pl.artlomako.taskprocessor.task.TaskQueue;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private static final int QUEUE_SIZE_LIMIT = 1000;
+    private static final int PRODUCERS_COUNT = 2;
+    private static final int CONSUMERS_COUNT = 4;
 
     public static void main(String[] args) {
-        LOGGER.info("Hello world!");
+        ExecutorService producersPool = Executors.newFixedThreadPool(PRODUCERS_COUNT);
+        ExecutorService consumersPool = Executors.newFixedThreadPool(CONSUMERS_COUNT);
+        TaskQueue taskQueue = new TaskQueue(QUEUE_SIZE_LIMIT);
+        for (int i = 0; i < 2; i++) {
+            producersPool.submit(new TaskProducer(taskQueue, TaskFactory::createWithRandomInfixExpression));
+        }
+
+        for (int i = 0; i < 4; i++) {
+            consumersPool.submit(new TaskConsumer(taskQueue));
+        }
     }
 }
