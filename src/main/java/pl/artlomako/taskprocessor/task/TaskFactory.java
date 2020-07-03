3c48abf32175d0b44d94f59318c0084fcd4d166a
Artur Lomako <lomako.artur@gmail.com>
2020-07-03 16:01:57 +0200

    Implement creation of tasks with math expression

--- src/main/java/pl/artlomako/taskprocessor/task/TaskFactory.java
+++ src/main/java/pl/artlomako/taskprocessor/task/TaskFactory.java
@@ -1,6 +1,17 @@
 package pl.artlomako.taskprocessor.task;
 
+import pl.artlomako.taskprocessor.math.InfixExpressionCalculator;
+import pl.artlomako.taskprocessor.math.InfixExpressionGenerator;
+
 public class TaskFactory {
+    public static Task createWithRandomInfixExpression() {
+        String expression = InfixExpressionGenerator.generate();
+        return new Task(
+                expression,
+                () -> InfixExpressionCalculator.calculate(expression)
+        );
+    }
+
     public static Task createDummy() {
         return new Task("", () -> {
 
